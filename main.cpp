bf5a237e65714e7d979bddaf58221b629ac9569b
lucasoeth <43838158+lucasoeth@users.noreply.github.com>
2023-04-27 23:13:52 +0200

    bet

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <Eigen/Sparse>
 #include <chrono>
 #include <iostream>
 #include <omp.h>
@@ -8,87 +9,118 @@ using namespace std::chrono;
 #define L 100
 #define V 100
 #define eps 1e-5
+#define O 201       // 1+2*L
+#define MAX_I 15050 // L(L+1)/2 + L*L
+#define OFFSET(y) ((y) * (y + O) / 2)
+
+typedef Eigen::SparseMatrix<float> Mat;
+typedef Eigen::Matrix<float, MAX_I, 1> Vec;
+typedef Eigen::Triplet<float> Triplet;
+
+int offset(int y) { return y * (y + O) / 2; }
 
 int main(int argc, char *argv[]) {
-  float phi[L * L + (L + 1) * L / 2] = {0};
-  float err = eps + 1;
+  Mat mat(MAX_I, MAX_I);
 
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
   // set boundary
+  Vec b = Vec::Zero();
 
   float slope = (float)V / (float)L;
 
   for (int x = 0; x < L + 1; ++x) {
-    phi[L - x] = x * slope;
+    b[L - x] = x * slope;
   }
 
-  high_resolution_clock::time_point t1 = high_resolution_clock::now();
+  // set matrix
 
-  int iter = 0;
+  // for y in range(1, L-1):
+  //   i = coord_to_index((y, 0), L)
 
-  while (err > eps) {
-    ++iter;
-    err = 0;
-#pragma omp parallel for shared(phi) schedule(static)
-    for (int y = 1; y < L - 1; ++y) {
-      int offset = y * L + y * (y + 1) / 2;
+  //   matrix[i, i] = -2
+  //   matrix[i, i+1] = 1
+  //   matrix[i, coord_to_index((y+1, 1), L)] = 1
 
-      // for (int x = 1; x < 2 * L - 1; ++x) {
-      float old = phi[offset];
+  //   for x in range(1, L+y):
+  //       i+=1
+  //       matrix[i, i] = -4
+  //       matrix[i, i+1] = 1
+  //       matrix[i, i-1] = 1
+  //       matrix[i, coord_to_index((y-1, x-1), L)] = 1
+  //       matrix[i, coord_to_index((y+1, x+1), L)] = 1
 
-      phi[offset] = 0.5 * (phi[offset + 1] + phi[offset + L + y + 2]);
+  //   write the above code in C++ using Eigen
 
-      float temp_err = phi[offset] - old;
+  int N = 0;
+  int insertions = 0;
 
-      if (temp_err > err) {
-        err = temp_err;
-      }
+  // mat.reserve(73059);
+  vector<Triplet> triplets;
+  triplets.reserve(73059);
+  for (int y = 1; y < L - 1; ++y) {
+    ++N;
+    int i = OFFSET(y);
 
-      for (int x = 1; x < L + 1 + y; ++x) {
-        float old = phi[offset + x];
+    triplets.push_back(Triplet(i, i, -2));
+    triplets.push_back(Triplet(i, i + 1, 1));
+    triplets.push_back(Triplet(i, OFFSET(y + 1) + 1, 1));
 
-        phi[offset + x] = 0.25 * (phi[offset + x + 1] + phi[offset + x - 1] +
-                                  phi[offset + x - (L + y + 1)] +
-                                  phi[offset + x + L + y + 2]);
+    // mat.insert(i, i) = -2;
+    // mat.insert(i, i + 1) = 1;
+    // mat.insert(i, OFFSET(y + 1) + 1) = 1;
 
-        float temp_err = phi[offset + x] - old;
+    insertions += 3;
 
-        if (temp_err > err) {
-          err = temp_err;
-        }
-      }
+    // #pragma omp parallel for shared(mat) schedule(static)
+    for (int x = 1; x < L + y; ++x) {
+      ++N;
+      // ++i;
+
+      triplets.push_back(Triplet(i + x, i + x, -4));
+      triplets.push_back(Triplet(i + x, i + x + 1, 1));
+      triplets.push_back(Triplet(i + x, i + x - 1, 1));
+      triplets.push_back(Triplet(i + x, OFFSET(y - 1) + x - 1, 1));
+      triplets.push_back(Triplet(i + x, OFFSET(y + 1) + x + 1, 1));
+
+      // mat.insert(i, i) = -4;
+      // mat.insert(i, i + 1) = 1;
+      // mat.insert(i, i - 1) = 1;
+      // mat.insert(i, OFFSET(y - 1) + x - 1) = 1;
+      // mat.insert(i, OFFSET(y + 1) + x + 1) = 1;
+
+      insertions += 5;
     }
   }
-  // #pragma omp parallel for shared(phi) schedule(static)
-  //     for (int x = 1; x < 2 * L - 1; ++x) {
-  //       for (int y = 1; y < L - 1; ++y) {
-  //         float old = phi[y][x];
-  //         phi[y][x] = 0.25 * (phi[y][x + 1] + phi[y][x - 1] + phi[y + 1][x]
-  //         +
-  //                             phi[y - 1][x]);
 
-  //         for (int x = 0; x < L - 1; x++) {
-  //           phi[0][x] = phi[L - 1 - x][L - 1];
-  //         }
+  cout << triplets.size() << endl;
 
-  //         float temp_err = phi[y][x] - old;
+  for (Triplet v : triplets) {
+    if (v.row() >= MAX_I || v.col() >= MAX_I || v.row() < 0 || v.col() < 0) {
+      cout << "ERROR" << endl;
+      cout << v.row() << " " << v.col() << " " << v.value() << endl;
+    }
+  }
 
-  //         if (temp_err > err) {
-  //           err = temp_err;
-  //         }
-  //       }
-  //     }
-
-  //     for (int x = 0; x < L - 1; x++) {
-  //       phi[0][x] = phi[L - 1 - x][L - 1];
-  //     }
-  //   }
+  mat.setFromTriplets(triplets.begin(), triplets.end());
 
   high_resolution_clock::time_point t2 = high_resolution_clock::now();
-  duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+  Vec x;
+  {
+    Eigen::BiCGSTAB<Mat, Eigen::IdentityPreconditioner> bicg;
+    bicg.compute(mat);
+    x = bicg.solve(b);
+    std::cout << "BiCGSTAB: #iterations: " << bicg.iterations()
+              << ", estimated error: " << bicg.error() << std::endl;
+  }
 
-  cout << iter << endl;
-  cout << err << endl;
-  std::cout << "It took me " << time_span.count() << " seconds." << endl;
+  high_resolution_clock::time_point t3 = high_resolution_clock::now();
+  duration<double> part = duration_cast<duration<double>>(t3 - t2);
+  duration<double> whole = duration_cast<duration<double>>(t3 - t1);
+
+  std::cout << "Whole program " << whole.count()*1000 << " ms." << endl;
+  std::cout << "Part  " << part.count()*1000 << " ms." << endl;
+  std::cout << "N  " << N << endl;
+  std::cout << "insertions  " << insertions << endl;
 
   return 0;
 }
---
+++ main_iterative.cpp
@@ -0,0 +1,100 @@
+#include <chrono>
+#include <iostream>
+#include <omp.h>
+
+using namespace std;
+using namespace std::chrono;
+
+#define L 100
+#define V 100
+#define eps 1e-5
+
+int main(int argc, char *argv[]) {
+  float phi[L * L + (L + 1) * L / 2] = {0};
+  float err = eps + 1;
+
+  // set boundary
+
+  float slope = (float)V / (float)L;
+
+  for (int x = 0; x < L + 1; ++x) {
+    phi[L - x] = x * slope;
+  }
+
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
+
+  int iter = 0;
+  int extra_iter = 0;
+
+  while (err > eps) {
+    ++iter;
+    err = 0;
+#pragma omp parallel for shared(phi) schedule(static)
+    for (int y = 1; y < L - 1; ++y) {
+      int offset = y * L + y * (y + 1) / 2;
+
+      // for (int x = 1; x < 2 * L - 1; ++x) {
+      float old = phi[offset];
+
+      phi[offset] = 0.5 * (phi[offset + 1] + phi[offset + L + y + 2]);
+
+      float temp_err = phi[offset] - old;
+
+      if (temp_err > err) {
+        err = temp_err;
+      }
+
+      for (int x = 1; x < L + 1 + y; ++x) {
+        float old = phi[offset + x];
+
+        phi[offset + x] = 0.25 * (phi[offset + x + 1] + phi[offset + x - 1] +
+                                  phi[offset + x - (L + y + 1)] +
+                                  phi[offset + x + L + y + 2]);
+
+        float temp_err = phi[offset + x] - old;
+        
+        if (temp_err < err && iter < 17000) {
+          extra_iter++;
+        }
+
+        if (temp_err > err) {
+          err = temp_err;
+        }
+      }
+    }
+  }
+  // #pragma omp parallel for shared(phi) schedule(static)
+  //     for (int x = 1; x < 2 * L - 1; ++x) {
+  //       for (int y = 1; y < L - 1; ++y) {
+  //         float old = phi[y][x];
+  //         phi[y][x] = 0.25 * (phi[y][x + 1] + phi[y][x - 1] + phi[y + 1][x]
+  //         +
+  //                             phi[y - 1][x]);
+
+  //         for (int x = 0; x < L - 1; x++) {
+  //           phi[0][x] = phi[L - 1 - x][L - 1];
+  //         }
+
+  //         float temp_err = phi[y][x] - old;
+
+  //         if (temp_err > err) {
+  //           err = temp_err;
+  //         }
+  //       }
+  //     }
+
+  //     for (int x = 0; x < L - 1; x++) {
+  //       phi[0][x] = phi[L - 1 - x][L - 1];
+  //     }
+  //   }
+
+  high_resolution_clock::time_point t2 = high_resolution_clock::now();
+  duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+
+  cout << extra_iter << endl;
+  cout << iter << endl;
+  cout << err << endl;
+  std::cout << "It took me " << time_span.count() << " seconds." << endl;
+
+  return 0;
+}
